016a05a605c8979b2c75d7b08cced9b7cc7460b2
Vincil Bishop <vincil.bishop@vbishop.com>
2015-09-22 11:45:44 -0500

    Initial commit

---
+++ Example/Pods/Target Support Files/CoreRestMapping/CoreRestMapping-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double CoreRestMappingVersionNumber;
+FOUNDATION_EXPORT const unsigned char CoreRestMappingVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-CoreRestMapping_Tests/Pods-CoreRestMapping_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreRestMapping_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CoreRestMapping_Tests
+@end
